840e8c0f62f03ba52d095acaaedd00f77e53fe05
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-12-17 21:57:04 +0800

    feat: calculate ammount

--- packages/core/contracts/Exchange.sol
+++ packages/core/contracts/Exchange.sol
@@ -11,12 +11,34 @@ contract Exchange {
         tokenAddress = _tokenAddress;
     }
 
-    function getReverse() public view returns (uint256) {
+    function getReserve() public view returns (uint256) {
         return IERC20(tokenAddress).balanceOf(address(this));
     }
 
     function addLiquidity(uint256 _tokenAmount) public payable {
         IERC20 token = IERC20(tokenAddress);
         token.transferFrom(msg.sender, address(this), _tokenAmount);
+    }
+
+    // (inputReverse + inputAmount) * (outputReverse - outputAmount) = inputReverse * outputReverse
+    function getAmount(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) private pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+        return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+    }
+
+    function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
+        require(_ethSold > 0, "ethSold is too small");
+        uint256 tokenReserve = getReserve();
+        return getAmount(_ethSold, address(this).balance, tokenReserve);
+    }
+
+    function getEthAmount(uint256 _tokenSold) public view returns (uint256) {
+        require(_tokenSold > 0, "tokenSold is too small");
+        uint256 tokenReserve = getReserve();
+        return getAmount(_tokenSold, tokenReserve, address(this).balance);
     }
 }
--- packages/core/test/Exchange.spec.ts
+++ packages/core/test/Exchange.spec.ts
@@ -6,7 +6,7 @@ const toWei = (value: number) => ethers.parseEther(value.toString())
 const fromWei = (value: string | number) =>
   ethers.formatEther(typeof value === 'string' ? value : value.toString())
 
-const getBalance = ethers.provider.getBalance
+const getBalance = ethers.provider.getBalance.bind(ethers.provider)
 
 describe('addLiquidity', async () => {
   // todo: make contract instance type safe
@@ -21,22 +21,49 @@ describe('addLiquidity', async () => {
     user = signers[1]
     const Token = await ethers.getContractFactory('Token')
     token = await Token.deploy('Token', 'TKN', toWei(1000000))
-    await token.deployed()
+    await token.waitForDeployment()
 
     const Exchange = await ethers.getContractFactory('Exchange')
-    exchange = await Exchange.deploy(token.address)
-    await exchange.deployed()
+    exchange = await Exchange.deploy(token.target)
+    await exchange.waitForDeployment()
   })
 
   it('is deployed', async () => {
-    expect(await exchange.deployed()).to(exchange)
+    expect(await exchange.waitForDeployment()).to.equal(exchange)
   })
 
-  it('adds liquidity', async () => {
-    await token.approve(exchange.address, toWei(200))
-    await exchange.addLiquidity(toWei(200), { value: toWei(100) })
+  describe('addLiquidity', async () => {
+    it('adds liquidity', async () => {
+      await token.approve(exchange.target, toWei(200))
+      await exchange.addLiquidity(toWei(200), { value: toWei(100) })
+      expect(await getBalance(exchange.target)).to.equal(toWei(100))
+      expect(await exchange.getReserve()).to.equal(toWei(200))
+    })
+  })
 
-    expect(await getBalance(exchange.address)).to.equal(toWei(100))
-    expect(await exchange.getReserve()).to.equal(toWei(200))
+  describe('getTokenAmount', async () => {
+    it('returns correct token amount', async () => {
+      await token.approve(exchange.target, toWei(2000))
+      await exchange.addLiquidity(toWei(2000), { value: toWei(1000) })
+      let tokenAmount = await exchange.getTokenAmount(toWei(1))
+      expect(fromWei(tokenAmount)).to.equal('1.998001998001998001')
+      tokenAmount = await exchange.getTokenAmount(toWei(100))
+      expect(fromWei(tokenAmount)).to.equal('181.818181818181818181')
+      tokenAmount = await exchange.getTokenAmount(toWei(1000))
+      expect(fromWei(tokenAmount)).to.equal('1000.0')
+    })
+  })
+
+  describe('getEthAmount', async () => {
+    it('returns correct eth amount', async () => {
+      await token.approve(exchange.target, toWei(2000))
+      await exchange.addLiquidity(toWei(2000), { value: toWei(1000) })
+      let ethAmount = await exchange.getEthAmount(toWei(2))
+      expect(fromWei(ethAmount)).to.equal('0.999000999000999')
+      ethAmount = await exchange.getEthAmount(toWei(100))
+      expect(fromWei(ethAmount)).to.equal('47.619047619047619047')
+      ethAmount = await exchange.getEthAmount(toWei(2000))
+      expect(fromWei(ethAmount)).to.equal('500.0')
+    })
   })
 })
