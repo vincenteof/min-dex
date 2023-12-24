8aa6503a8beefbe2e231b10ae64ac349e54952be
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-12-24 23:28:21 +0800

    feat: add contracts object

--- packages/frontend/lib/contract.ts
+++ packages/frontend/lib/contract.ts
@@ -1,7 +1,7 @@
-import {
-  Abi,
-} from 'abitype'
+import { Abi } from 'abitype'
 import { Address } from 'viem'
+import deployedContracts from '@/contracts/deployedContracts'
+import externalContracts from '@/contracts/externalContracts'
 
 export type InheritedFunctions = { readonly [key: string]: string }
 export type GenericContract = {
@@ -15,3 +15,24 @@ export type GenericContractsDeclaration = {
     [contractName: string]: GenericContract
   }
 }
+
+const deepMergeContracts = <
+  D extends Record<PropertyKey, any>,
+  S extends Record<PropertyKey, any>
+>(
+  destination: D,
+  source: S
+) => {
+  const result: Record<PropertyKey, any> = {}
+  const allKeys = Array.from(
+    new Set([...Object.keys(source), ...Object.keys(destination)])
+  )
+  for (const key of allKeys) {
+    result[key] = { ...destination[key], ...source[key] }
+  }
+  return result
+}
+
+const contractsData = deepMergeContracts(deployedContracts, externalContracts)
+
+export const contracts = contractsData as GenericContractsDeclaration | null
